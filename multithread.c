812fd6e9a45a4e253a9adcd4b175dfb560b91059
Hunter Carroll <carrollh@msoe.edu>
2025-11-23 23:43:12 -0600

    double buffer implementation with threads turney example

--- multithread.c
+++ multithread.c
@@ -22,94 +22,118 @@
 #define SEM_NAME4 "/my_sem4"
 #define SHM_SIZE 1024
 
+typedef struct {
+    int* shm_addr;
+    sem_t* sem_write_a;
+    sem_t* sem_write_b;
+    sem_t* sem_read_a;
+    sem_t* sem_read_b;
+    sem_t* sem_mutex;
+} thread_data_t;
+
+void* writer_thread(void* arg);
+void* reader_thread(void* arg);
+
 int main() {
+    // Initialize double buffer
     int shm_fd;
     int *shm_addr;
-    sem_t *sem1;
-    sem_t *sem2;
-    sem_t *sem3;
-    sem_t *sem4;
-    pid_td pid;
+
+    // Initialize semaphores
+    sem_t sem_write_a;
+    sem_t sem_write_b;
+    sem_t sem_read_a;
+    sem_t sem_read_b;
+    sem_t sem_mutex;
+
+    // Initialize x value for calculations
     int x = 10;
-    sem_unlink(SEM_NAME1);
-    sem_unlink(SEM_NAME2);
-    sem_unlink(SEM_NAME3);
-    sem_unlink(SEM_NAME4);
-    
-    // Create shared memory
+
+    // unlink existing shared memory
+    shm_unlink(SHM_NAME);
+
+    // Create shared memory and map
     shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        perror("shm_open");
-        exit(1);
-    }
-
-    // Set size of shared memory
-    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
-        perror("ftruncate");
-        exit(1);
-    }
-
-    // Map the shared memory
+    ftruncate(shm_fd, SHM_SIZE);
     shm_addr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shm_addr == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
 
-    // Create semaphores
-    sem1 = sem_open(SEM_NAME1, O_CREAT | O_RDWR, 0666, 0);
-    sem2 = sem_open(SEM_NAME2, O_CREAT | O_RDWR, 0666, 0);
-    sem3 = sem_open(SEM_NAME3, O_CREAT | O_RDWR, 0666, 0);
-    sem4 = sem_open(SEM_NAME4, O_CREAT | O_RDWR, 0666, 0);
-    if (sem1 == SEM_FAILED || sem2 == SEM_FAILED || sem3 == SEM_FAILED || sem4 == SEM_FAILED) {
-        perror("sem_open");
-        exit(1);
-    }
+    sem_init(&sem_write_a, 0, 1);
+    sem_init(&sem_write_b, 0, 1);
+    sem_init(&sem_read_a, 0, 0);
+    sem_init(&sem_read_b, 0, 0);
+    sem_init(&sem_mutex, 0, 1);
 
-    // Fork a child process
-    int sval;
-    pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    }
+    thread_data_t thread_data = {
+        shm_addr,
+        &sem_write_a,
+        &sem_write_b,
+        &sem_read_a,
+        &sem_read_b,
+        &sem_mutex
+    };
+
+    // Note:
+    // Buffer 0: *shm_addr
+    // Buffer 1: *(shm_addr + 32 * sizeof(int))
+
+    pthread_t writer;
+    pthread_t reader;
+    pthread_create(&writer, NULL, writer_thread, &thread_data);
+    pthread_create(&reader, NULL, reader_thread, &thread_data);
+
+    pthread_join(writer, NULL);
+    pthread_join(reader, NULL);
+
+    munmap(shm_addr, SHM_SIZE);
+    close(shm_fd);
+    shm_unlink(SHM_NAME);
+    sem_destroy(&sem_write_a);
+    sem_destroy(&sem_write_b);
+    sem_destroy(&sem_read_a);
+    sem_destroy(&sem_read_b);
+    sem_destroy(&sem_mutex);
+
+    // return to exit
+    return 0;
+}
+
+void* writer_thread(void* arg) {
+    thread_data_t* data = (thread_data_t*)arg;
+    int* shm_addr = data->shm_addr;
+    int x = 10;
+
     for (int i = 0; i < 10; i++) {
-        // child process
-        if (i % 2 == 0) {
-            sem_wait(sem2);
+        if (i % 2 == 0) { // filters out half of the threads
+            sem_wait(data->sem_write_a);
             *shm_addr = x + i;
-            //sleep(1);
-            sem_post(sem1);
+            sem_post(data->sem_read_a);
             printf("wrote buffer 0\n");
         } else {
-            //printf("I am at sem_wait(sem4)\n");
-            sem_getvalue(sem4, &sval);
-            //printf("sem4 value: %d\n", sval);
-            sem_wait(sem4);
-            *(shm_addr+32*sizeof(int)) = x + i;
-            //sleep(1);
-            sem_post(sem3);
+            sem_wait(data->sem_write_b);
+            *(shm_addr + 32 * sizeof(int)) = x + i;
+            sem_post(data->sem_read_b);
             printf("wrote buffer 1\n");
         }
         sleep(1);
+    }
+    return NULL;
+}
 
-        //exit(0);
-    } else {
-        // parent process
-        if (i > 0) {
-            if(i % 2 == 0) {
-                //printf("I am at sem_wait(sem3)\n");
-                sem_wait(sem3);
-                printf("Parent reads buffer 1:%d\n\n", *(shm_addr+32*sizeof(int)));
-                //sleep(1);
-                sem_post(sem4);
-            } else {
-                sem_wait(sem1);
-                printf("Parent reads buffer 0: %d\n\n", *shm_addr);
-                //sleep(1);
-                sem_post(sem2);
-            }
+void* reader_thread(void* arg) {
+    thread_data_t* data = (thread_data_t*)arg;
+    int* shm_addr = data->shm_addr;
+    
+    for (int i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            sem_wait(data->sem_read_a);
+            printf("Parent reads buffer 0: %d\n\n", *shm_addr);
+            sem_post(data->sem_write_a);
+        } else {
+            sem_wait(data->sem_read_b);
+            printf("Parent reads buffer 1: %d\n\n", *(shm_addr + 32 * sizeof(int)));
+            sem_post(data->sem_write_b);
         }
         sleep(1);
     }
+    return NULL;
 }
